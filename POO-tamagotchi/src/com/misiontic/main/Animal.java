e7cb004d631bd80017815e23ae1d06e18f7fcf77
David <dsbpim@gmail.com>
2021-08-04 13:00:07 -0500

    Adding up the setters and getters

--- POO-tamagotchi/src/com/misiontic/main/Animal.java
+++ POO-tamagotchi/src/com/misiontic/main/Animal.java
@@ -1,103 +1,105 @@
 package com.misiontic.main;
 
 import java.awt.Color;
-/*public class Animal {
-    
-    float peso;//peso ideal de  4kg
-    int felicidad;//va de 0 a 100, 0 es triste; 100 es feliz.
-    int salud;//va de 0 a 100, 0 muerto, 100 muy saludable.
-    int hambre;//va de 0 a 100, 0 es sin hambre y 100 es famelico.
-    int sueno; //va de 0 a 100, 0 es muy despierto y 100 es muy somnoliento
-    
-    //Tengo que dar una respuesta cuando termine de comer?
-    //Yo quiero decir si ya termine de comer
-    private void comer() {
-        if(hambre <= 5) {
-            hambre = 0;
-            System.out.println("Estoy demasiado lleno, me voy a enfermar de tanto comer");
-            salud = salud -2;
-            peso +=2;
-            sueno += 2;
-            felicidad -= 3;
-           /* if(salud <0) {
-            	salud =0;
-            	System.out.println("Me estoy muriendo!");
-            }
-            if (peso > 4) {
-            	salud -=1;
-            	System.out.println("Debo hacer ejercicio!");
-            }
-            if(sueno >= 80) {
-            	salud -=1;
-            	System.out.println("Debo dormir un rato");
-            }
-            if(felicidad < 50) {
-            	salud -=1;
-            	System.out.println();
-            }
-            
-        }else {
-            hambre = hambre - 5;
-            peso +=1;
-            felicidad += 3;
-            sueno += 3;
-            salud += 3;
-        }
-    }
-    private void dormir() {
-    	if(sueno <= 5) {
-    		sueno = 0;
-    		System.out.println("He dormido más que suficiente!");
-    		salud -=1;
-    		peso +=1;
-    	}else {
-    		sueno -=5;
-    		peso +=1;
-    	}
-        
-    }
-    private void ejercitar() {
-    	if(peso > 4) {
-    		salud -=1;
-    		felicidad -= 2;
-    		if (salud<0) {
-    			salud =0;
-    		}
-    		
-    		System.out.println("Debo seguir haciendo ejercicio hasta llegar a 4kg!");
-    		peso -= 1;
-    		
-    	}else if(peso >3.5 && peso<=4) {
-    		salud +=2;
-    		felicidad += 2;
-    		System.out.println("Es suficiente ejercicio por ahora");
-    	}
-        
-    }
-    public int jugar() {
-    	if(felicidad <50) {
-    		salud -= 1;
-    		felicidad +=2;
-    		System.out.println("Necesito jugar para sentirme mejor!");
-    	}else {
-    		System.out.println("He jugado suficiente");
-    		salud += 1;
-    		
-    	}
-    	return felicidad;
-        
-    }
-*/
+
 
 public class Animal {
 	
-	float weight;
-	int happiness;//va de 0 a 100, 0 es triste; 100 es feliz.
-	int health;//va de 0 a 100, 0 muerto, 100 muy saludable.
-	int hungry;//va de 0 a 100, 0 es sin hambre y 100 es famelico.
-	int sleep; //va de 0 a 100, 0 es muy despierto y 100 es muy somnoliento
-	boolean awake;
+	private float weight;
+	private int happiness;//va de 0 a 100, 0 es triste; 100 es feliz.
+	private int health;//va de 0 a 100, 0 muerto, 100 muy saludable.
+	private int hungry;//va de 0 a 100, 0 es sin hambre y 100 es famelico.
+	private int sleep; //va de 0 a 100, 0 es muy despierto y 100 es muy somnoliento
+	private boolean awake;
 	
+	
+	
+	
+	public Animal(float weight, int happiness) {// Este constructor obliga al usuario a dale valores a weight y happiness
+		this( weight, happiness, 0);// con el this hace referencia al siguiente constructor, le entrega los parametros
+		// que le llegan al siguiente constructor e inicializa el otro parametro en 0(health)
+		this.weight = weight;
+		this.happiness = happiness;
+		
+	}
+	
+	public Animal(float weight, int happiness, int health) {
+		this(weight, happiness, health,0);
+		this.weight = weight;
+		this.happiness = happiness;
+		this.health = health;
+	}
+	
+
+	public Animal(float weight, int happiness, int health, int hungry) {
+		this(weight, happiness, health, hungry, 0,false);
+		this.weight = weight;
+		this.happiness = happiness;
+		this.health = health;
+		this.hungry = hungry;
+	}
+	
+
+	public Animal(float weight, int happiness, int health, int hungry, int sleep, boolean awake) {
+		super();// Este constructor es el ultimo llamado, quien inicializa todas las variables, ya sean obligadas
+		// siendo el caso que se utiliza este constructor, o si es de los primeros, inicializa los otros
+		// atributos con valores por defecto, ya sea en 0 o falso
+		this.weight = weight;
+		this.happiness = happiness;
+		this.health = health;
+		this.hungry = hungry;
+		this.sleep = sleep;
+		this.awake = awake;
+	}
+
+	public float getWeight() {
+		return weight;
+	}
+
+	public void setWeight(float weight) {
+		this.weight = weight;
+	}
+
+	public int getHappiness() {
+		return happiness;
+	}
+
+	public void setHappiness(int happiness) {
+		this.happiness = happiness;
+	}
+
+	public int getHealth() {
+		return health;
+	}
+
+	public void setHealth(int health) {
+		this.health = health;
+	}
+
+	public int getHungry() {
+		return hungry;
+	}
+
+	public void setHungry(int hungry) {
+		this.hungry = hungry;
+	}
+
+	public int getSleep() {
+		return sleep;
+	}
+
+	public void setSleep(int sleep) {
+		this.sleep = sleep;
+	}
+
+	public boolean isAwake() {
+		return awake;
+	}
+
+	public void setAwake(boolean awake) {
+		this.awake = awake;
+	}
+
 	//Tengo que dar una respuesta cuando termine de comer?
 	//Yo quiero decir si ya termine de comer
 	public void eat(Food food) {
--- POO-tamagotchi/src/com/misiontic/main/Main.java
+++ POO-tamagotchi/src/com/misiontic/main/Main.java
@@ -26,22 +26,27 @@ public class Main {
 		
 */
 		
-		Animal cat = new Animal();		
+		/*Animal cat = new Animal();		
 		cat.weight = 10;
 		cat.happiness = 100;//va de 0 a 100, 0 es triste; 100 es feliz.
 		cat.health = 100;//va de 0 a 100, 0 muerto, 100 muy saludable.
 		cat.hungry = 0;//va de 0 a 100, 0 es sin hambre y 100 es famelico.
 		cat.sleep = 0; //va de 0 a 100, 0 es muy despierto y 100 es muy somnoliento
-
+*/
+		Animal perro = new Animal(5, 4, 6, 7, 8, true);
+		System.out.println("El perro tiene las siguientes caracteristicas: " + perro.isAwake());
+		System.out.println("El perro tiene hambre=?: "+ perro.getHungry());
 		
-		Veterinary veterinary = new Veterinary();
-		Medicine m = veterinary.prescribeMedicine(cat);
+		
+		
+/*		Veterinary veterinary = new Veterinary();
+		Medicine m = veterinary.prescribeMedicine(perro);
 		System.out.println("la medicina que me recetan es: " + m.name);
 		
-		System.out.println("hambre gatico: " + cat.hungry);
+		System.out.println("hambre gatico: " + perro.hungry);
 		System.out.println(Math.floor((100 - 50)/10));
 		System.out.println(Math.ceil(5.25));
-
+*/
 
 	}
 
